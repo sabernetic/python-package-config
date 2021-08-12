45ce1043c407228e1578b279b0628750426e946f
Muntashir <islam.muntashir@gmail.com>
2021-08-11 20:56:02 -0600

    Add action

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@
 cp .pypirc $HOME/
 mkdir -p $HOME/.pip
 cp pip.conf $HOME/.pip/
-export token=$(aws codeartifact get-authorization-token --domain hishab --domain-owner 140370042521 --query authorizationToken --output text)
-pip config set global.index-url https://aws:$token@hishab-140370042521.d.codeartifact.us-east-1.amazonaws.com/pypi/mypy/simple/
+export token=$(aws codeartifact get-authorization-token --domain sabernetic --domain-owner 538326023882 --query authorizationToken --output text)
+pip config set global.index-url https://aws:$token@sabernetic-538326023882.d.codeartifact.us-east-1.amazonaws.com/pypi/mypy/simple/
