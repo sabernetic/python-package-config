f86dbee727ab042b4900d31fbb61305c7556d79f
trithai1 <trithai45@gmail.com>
2022-12-09 11:16:40 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-cp .pypirc $HOME/
-mkdir -p $HOME/.pip
-cp pip.conf $HOME/.pip/
-export token=$(aws codeartifact get-authorization-token --domain sabernetic --domain-owner 538326023882 --query authorizationToken --output text)
-pip config set global.index-url https://aws:$token@sabernetic-538326023882.d.codeartifact.us-east-1.amazonaws.com/pypi/mypy/simple/
+pip install -r requirements.txt
