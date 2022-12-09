915c77f468127570ac4387e8ffd1e82f5473d10e
trithai1 <trithai45@gmail.com>
2022-12-09 13:33:40 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,19 @@
-echo $1
+acc_id=$1
+region=$2
+domain=$3
+repos=$4
 
-pip install -r requirements.txt
+install_cmd="pip install -r requirements.txt"
+if [ -z "$acc_id" ] || [ -z "$region" ] || [ -z "$domain" ] || [ -z "$repos" ]; then
+  $install_cmd
+  exit 0
+fi
+
+export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain $domain --domain-owner $acc_id --region $region --query authorizationToken --output text`
+
+for repo in $repos
+do
+  install_cmd="$install_cmd --extra-index-url https://aws:$CODEARTIFACT_AUTH_TOKEN@$domain-$acc_id.d.codeartifact.$region.amazonaws.com/pypi/$repo/simple/"
+done
+
+$install_cmd
